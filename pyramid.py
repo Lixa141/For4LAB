fdcd7fb04255163f8956dfc4d84e1b3de363a723
Рубцов Илья <Rubthov-Ilya1997@yandex.ru>
2017-01-20 20:54:00 +0500

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,22 +1,25 @@
-from jinja2 import Environment, FileSystemLoader, Template
 from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+from jinja2 import Environment, FileSystemLoader,Template
 
 env = Environment(loader=FileSystemLoader('.'))
 
-def app(env, start_resp):
-    start_resp('200 OK', [('Content-Type', 'text/html')])
-    path = env['PATH_INFO']
+def index(request):
+     template = env.get_template('index.html').render(link="""<a href="http://localhost:8000/about/aboutme.html">Index </a>""")
+return Response(template)
 
-    def template(link):
-        return env.get_template(path).render(link=link)
+def about(request):
+    template = env.get_template('about/aboutme.html').render(link="""<a href="http://localhost:8000/index.html"> About me </a>""")    
+return Response(template)
 
-    if 'index.html' in path:
-        link = '<a href="/about/aboutme.html">about me</a>'
-
-    if 'about/aboutme.html' in path:
-        link = '<a href="/index.html">index</a>'
-
-    return [template(link).encode('utf-8')]
-
-serv = make_server('', 8000, app)
-serv.serve_forever()
+if __name__ == '__main__':
+    config = Configurator()
+    configurator.add_route("aboutme",'/about/aboutme.html')
+    configurator.add_view(AboutMe,route_name='aboutme')
+    configurator.add_route('index', '/index.html')
+    configurator.add_view(Index, route_name='index')
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 8000, app)
+    server.serve_forever()
+    
